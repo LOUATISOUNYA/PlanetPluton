00ac348feaae63883b3d979417103e0d7ca59dd9
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-15 21:21:09 +0200

    Le cas de Pluton Version 2 - Opérateur d'incrémentation

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,13 +1,16 @@
 public class HelloUniverse {
 
-    public static void main(String... args) {
-        String chaine1 = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(chaine1);
-        int nbPlanetes = 9;
+    public static void main(String... args){
+        String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(phrase);
+        int nbPlanetes=8;
         System.out.println(nbPlanetes);
-        chaine1 = "Il y a quelques années cependant, elles étaient au nombre de :";
-        System.out.println(chaine1);
-        nbPlanetes = 8;
-        System.out.println(nbPlanetes);
+        phrase="Il y a quelques années cependant, elles étaient au nombre de : ";
+        System.out.println(phrase);
+        int nbPlanetesPluton=++nbPlanetes;
+        System.out.println(nbPlanetesPluton);
+
     }
+
+
 }
