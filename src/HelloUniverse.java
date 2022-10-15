e1a1ad0d82f998e3e40436a0273c698497e2c7a0
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-15 22:57:29 +0200

    Le cas de Pluton Version 4 - Opérateur de concaténation

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,21 +1,13 @@
 public class HelloUniverse {
 
     public static void main(String... args){
-       /* String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(phrase);
         int nbPlanetes=8;
-        System.out.println(nbPlanetes);
-        phrase="Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(phrase);
-        int nbPlanetesPluton=++nbPlanetes;
-        System.out.println(nbPlanetesPluton);*/
-        String maChaine= "bonjour :";
-        int age=30;
-        System.out.println(maChaine+(age++));//31
-        System.out.println(maChaine+(++age));//32
-        System.out.println(maChaine+(age+1));//33
+        String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :";
+        System.out.println(phrase+nbPlanetes);
+        nbPlanetes++;
+        phrase="Il y a quelques années cependant, elles étaient au nombre de :";
+        System.out.println(phrase+nbPlanetes);
 
     }
-
 
 }
