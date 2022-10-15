83f3292773e4a807b0f2812376b3d797d232e3c4
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-15 19:22:29 +0200

    Le cas de Pluton Version 2 - Première variable de type String

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,10 +1,13 @@
 public class HelloUniverse {
-    public static void main(String... args){
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :");
-        int nombreTotalPlanete = 8;
-        System.out.println(nombreTotalPlanete);
-        System.out.println("Il y a quelques années cependant, elles étaient au nombre de :");
-        nombreTotalPlanete=9;
-        System.out.println(nombreTotalPlanete);
+
+    public static void main(String... args) {
+        String chaine1 = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(chaine1);
+        int nbPlanetes = 9;
+        System.out.println(nbPlanetes);
+        chaine1 = "Il y a quelques années cependant, elles étaient au nombre de :";
+        System.out.println(chaine1);
+        nbPlanetes = 8;
+        System.out.println(nbPlanetes);
     }
 }
