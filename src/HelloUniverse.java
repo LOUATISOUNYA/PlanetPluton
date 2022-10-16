3c3602feb5293376b6b5a1036f4c0edc07ef327f
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-16 12:46:06 +0200

    Affichage conditionnel - switch-case-v1.0

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,16 +1,30 @@
 public class HelloUniverse {
     public static void main(String... args){
-       int age = 30;
-        System.out.printf("Mon age est %d ans .", age);
-        if (age > 30) {
-            System.out.println("oui, je fais plus jeune");
-        }
-         int ageDuCapitaine = 20;
-        if (ageDuCapitaine > 20 && age <50) {
-            System.out.println("L'age du capitaine est"+ ageDuCapitaine);
-            System.out.println("Hello");
-            System.out.println("World !");
-
-        }
+       int mois=7 ;
+       switch (mois) {
+           case 1 :
+               System.out.println("Janvier");
+               break;
+case 2 :
+               System.out.println("Février");
+               break;
+case 3 :
+                  System.out.println("Mars");
+                break;
+case 4 :
+                  System.out.println("Avril");
+                break;
+case 5 :
+                  System.out.println("Mai");
+                break;
+case 6 :
+                  System.out.println("Juin");
+                break;
+case 7 :
+                  System.out.println("Juillet");
+                break;
+case 8 :
+                  System.out.println("Aout");
+       }
     }
 }
