3b0fca2938ff255d4d367f9bf5c5021f354da2a3
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-16 22:03:46 +0200

    Affichage itératif- Pluton Planet - Object-Class

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,26 +1,42 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        int nbPlanetes=7;
-        while (nbPlanetes<10) {
-            switch (nbPlanetes) {
-                case 7:
-                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
-                    break;
-                case 8:
-                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
-                    break;
-                case 9:
-                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
-                    break;
-                default:
-                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanetes);
-            }
-            nbPlanetes++;
-        }
+                Planete planete1 = new Planete();
+                Planete planete2 = new Planete();
+                Planete planete3 = new Planete();
+                Planete planete4 = new Planete();
+                Planete planete5 = new Planete();
+                Planete planete6 = new Planete();
+                Planete planete7 = new Planete();
+                Planete planete8 = new Planete();
+                Planete planete9 = new Planete();
+                planete1.nom = "Mercure";
+                planete1.matiere = "Tellurique";
+                planete1.diametre = 4880;
+                planete2.nom = "Venus";
+                planete2.matiere = "Tellurique";
+                planete2.diametre = 12100;
+                planete3.nom = "Terre";
+                planete3.matiere = "Tellurique";
+                planete3.diametre = 12756;
+                planete4.nom = "Mars";
+                planete4.matiere = "Tellurique";
+                planete4.diametre = 6792;
+                planete5.nom = "Jupiter";
+                planete5.matiere = "Gazeuse";
+                planete5.diametre = 142984;
+                planete6.nom = "Saturne";
+                planete6.matiere = "Gazeuse";
+                planete6.diametre = 120536;
+                planete7.nom = "Uranus";
+                planete7.matiere = "Gazeuse";
+                planete7.diametre = 51118;
+                planete8.nom = "Neptune";
+                planete8.matiere = "Gazeuse";
+                planete8.diametre = 49532;
+                planete9.nom = "Pluton";
+                planete9.matiere = "Gazeuse";
+                planete9.diametre = 2300;
+                System.out.println(planete5.nom + " est une planète " + planete5.matiere + " avec un diamètre de " + planete1.diametre + " kilomètres.");
+
     }
 }
-
-/***Ennoncé de l'exercice
- * La structure itérative avec le mot clé while
- * Remplacez maintenant la "boucle" for par l'équivalent à base du mot clé while .
- ****/
