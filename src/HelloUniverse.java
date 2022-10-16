8051f36fcd6e8d10d26e21379869df778264d17b
LOUATI SOUNYA <sonia.louati1@esprit.tn>
2022-10-16 15:21:56 +0200

    Affichage itératif- Pluton Planet - While

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,10 +1,26 @@
 public class HelloUniverse {
-    public static void main(String... args){
-        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :");
-        int nombreTotalPlanete = 8;
-        System.out.println(nombreTotalPlanete);
-        System.out.println("Il y a quelques années cependant, elles étaient au nombre de :");
-        nombreTotalPlanete=9;
-        System.out.println(nombreTotalPlanete);
+    public static void main(String... args) {
+        int nbPlanetes=7;
+        while (nbPlanetes<10) {
+            switch (nbPlanetes) {
+                case 7:
+                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+                    break;
+                case 8:
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+                    break;
+                case 9:
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+                    break;
+                default:
+                    System.out.printf("Le programme ne peut pas fournir de résultat pour %d", nbPlanetes);
+            }
+            nbPlanetes++;
+        }
     }
 }
+
+/***Ennoncé de l'exercice
+ * La structure itérative avec le mot clé while
+ * Remplacez maintenant la "boucle" for par l'équivalent à base du mot clé while .
+ ****/
